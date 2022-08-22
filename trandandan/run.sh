39cf70edc30c35747480c5fd40d1cc2dfb97c1b4
Abdelhakim Nassif <anassif@e1r5p5.1337.ma>
2022-08-22 20:52:53 +0100

    start

---
+++ trandandan/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm install
+npm run start:dev
+
+tail -f
---
+++ trandandan/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    entities: ['dist/**/*.entity.{ts,js}'],
+    migrations: ['dist/migrations/*.{ts,js}'],
+    migrationsTableName: 'typeorm_migrations',
+    logger: 'file',
+    synchronize: true, // never use TRUE in productio
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
