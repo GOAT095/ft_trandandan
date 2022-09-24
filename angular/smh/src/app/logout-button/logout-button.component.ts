d0be34f090a8b5d013541752837d6e346d839af2
hchandad <hchandad@student.1337.ma>
2022-09-24 15:18:42 +0000

    add logout-button component

---
+++ angular/smh/src/app/logout-button/logout-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-button',
+  templateUrl: './logout-button.component.html',
+  styleUrls: ['./logout-button.component.less']
+})
+export class LogoutButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
