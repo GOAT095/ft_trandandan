a0ddc68be1f1b9df2d85f7a7f8ea28198a651893
hchandad <hchandad@student.1337.ma>
2022-09-24 15:19:45 +0000

    Use a proper title

--- angular/smh/src/app/app.component.ts
+++ angular/smh/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'smh';
+  title = 'ft_transcendence';
+  constructor(public api: ApiService) {}
 }
