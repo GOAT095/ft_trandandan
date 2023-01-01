7295d6042d72dcab11976ff84e3891ea05e9976a
GOAT095 <hakimoxxx@gmail.com>
2023-01-02 00:05:13 +0100

    forgot;

--- trt/src/api/game/game.entity.ts
+++ trt/src/api/game/game.entity.ts
@@ -12,12 +12,12 @@ export class Gamehistoryclass{
   id: number; /** game id **/
 
 
-  @ManyToOne(() => User, (user) => user.p2, {onDelete: 'CASCADE'})
+  @ManyToOne(() => User, (User) => User.p2, {onDelete: 'CASCADE'})
   playerOne: User; /** playerOne not just id but the all player data btw a achraf**/ 
 
-  @ManyToOne(() => User, (user) => user.p2, {onDelete: 'CASCADE'}) //cascade to delete child if parent is deleted
+  @ManyToOne(() => User, (User) => User.p2, {onDelete: 'CASCADE'}) //cascade to delete child if parent is deleted
   playerTwo: User; /** playerTwo  **/
 
   @CreateDateColumn()
-  createdDate: Date
+  createdDate: Date;
 }
