4bb76c125e0465c95f829367447ba7f20380a58a
hchandad <hchandad@student.1337.ma>
2023-01-04 18:52:34 +0000

    update environment.example.ts

--- client/client/src/environments/environment.example.ts
+++ client/client/src/environments/environment.example.ts
@@ -2,12 +2,20 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/*
+Update with: apiUrl, oauthUrl see environment.example.ts
+*/
+
 export const environment = {
-  baseUrl: "",
-  apiUrl: "", // fill with the full backend url e.i: "http://localhost:4200/api"
-  oauthUrl: "", // fill with the redirect url from the Intra Api dashboard e.i:
-                //  "https://api.intra.42.fr/oauth/authorize?client_id=<CLIENT_ID>&redirect_uri=<REDIRECT_URI>&response_type=code"
-  githubOAuthUrl: "",
+  /*
+  oauthUrl: "<fill with redirect url from intra api dashboard>",
+  */
+  // example:
+  //apiUrl: "http://localhost:4200/api",
+  //baseUrl: "https://ft_trandandan.example",
+  //apiUrl: "https://ft_trandandan.example/api",
+  //oauthUrl: "https://api.intra.42.fr/oauth/authorize?client_id=<client_id>&redirect_uri=<redirect_uri>&response_type=code",
+  //githubOAuthUrl: "https://github.com/login/oauth/authorize?client_id=<client_id>",
   chatRefreshTime: 1500,
   production: false
 };
